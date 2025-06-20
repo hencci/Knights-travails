9149edd835e290362b167bb7e636844ce72b039e
hencci <hencci20@gmail.com>
2025-06-20 12:04:56 +0100

    Select DOM

---
+++ ui.js
@@ -0,0 +1,7 @@
+import { knightMoves } from "./pathfinders.js";
+
+const boardElement = document.getElementById('board');
+const resultElement = document.getElementById('result');
+const form = document.getElementById('moveForm');
+const startInput = document.getElementById('startInput');
+const endInput = document.getElementById('endInput');
