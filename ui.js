0f9ad8715ded15fb6a4643357e5e2dcac8c0347c
hencci <hencci20@gmail.com>
2025-06-20 12:18:42 +0100

    Create function to create board

--- ui.js
+++ ui.js
@@ -5,3 +5,15 @@ const resultElement = document.getElementById('result');
 const form = document.getElementById('moveForm');
 const startInput = document.getElementById('startInput');
 const endInput = document.getElementById('endInput');
+
+function createBoard() {
+    boardElement.innerHTML = '';
+    for (let y = 7; y >= 0; y--) {
+        for (let x = 0; x < 8; x++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.dataset.pos = `${x},${y}`;
+            boardElement.appendChild(square);
+        }
+    }
+}
